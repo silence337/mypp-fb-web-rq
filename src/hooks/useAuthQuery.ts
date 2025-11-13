f03196ee833384e0b9f9b19a79b0531b25b87271
silence337 <silence337@naver.com>
2025-11-13 14:52:52 +0900

    훅 success 시, 스토어 변경

--- src/hooks/useAuthQuery.ts
+++ src/hooks/useAuthQuery.ts
@@ -7,59 +7,48 @@ import {
 } from '@/api/authApi';
 import type { FormValues } from '@/types/formTypes';
 import type { AuthUser, AuthUserLogin } from '@/types/authTypes';
-
-// export const useCurrentUser = () => {
-//   // AuthListener 사용자 데이터를 읽어옴
-//   return useQuery<AuthUser | null>({
-//     queryKey: ['user'],
-//     queryFn: () => new Promise(() => {}),
-//     staleTime: Infinity,
-//     gcTime: Infinity,
-//   });
-// };
-// onAuthStateChanged 에서 유저 상태를 감지하기 때문에 invalidateQueries 는 불필요.
+import { useAuthStore } from '@/store/authStore';
 
 // 회원가입
 export const useSignUp = () => {
-  //const queryClient = useQueryClient();
+  const setUser = useAuthStore((state) => state.setUser);
   return useMutation<AuthUser, Error, FormValues>({
     mutationFn: signUp,
-    // onSuccess: (user) => {
-    //   queryClient.setQueryData(['user'], user);
-    // },
+    onSuccess: (user) => {
+      setUser(user);
+    },
   });
 };
 
 // 로그인
 export const useSignIn = () => {
-  //const queryClient = useQueryClient();
+  const setUser = useAuthStore((state) => state.setUser);
   return useMutation<AuthUser, Error, AuthUserLogin>({
     mutationFn: userLogin,
-    // onSuccess: (user) => {
-    //   console.log(user);
-    //   queryClient.setQueryData(['user'], user);
-    // },
+    onSuccess: (user) => {
+      setUser(user);
+    },
   });
 };
 
 // 로그아웃
 export const useSignOut = () => {
-  //const queryClient = useQueryClient();
+  const setUser = useAuthStore((state) => state.setUser);
   return useMutation({
     mutationFn: userLogout,
-    // onSuccess: () => {
-    //   queryClient.setQueryData(['user'], null);
-    // },
+    onSuccess: () => {
+      setUser(null);
+    },
   });
 };
 
 // 회원탈퇴
 export const useDeleteUser = () => {
-  //const queryClient = useQueryClient();
+  const setUser = useAuthStore((state) => state.setUser);
   return useMutation<void, Error, string>({
     mutationFn: deleteCurrentUser,
-    // onSuccess: () => {
-    //   queryClient.setQueryData(['user'], null);
-    // },
+    onSuccess: () => {
+      setUser(null);
+    },
   });
 };
