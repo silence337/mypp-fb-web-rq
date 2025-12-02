29234b69cd4eb754b71ac115a2f4e0ff6ce833d3
silence337 <silence337@naver.com>
2025-12-02 17:36:06 +0900

    lottie 주석처리

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,27 +1,27 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import AuthButton from '../../components/auth/AuthButton'; // 로그인, 로그아웃 버튼
-import Lottie from 'lottie-web/build/player/lottie_light';
-import spaceAnimation from '@/assets/Rocket_loader.json'; // lottie free animation
+//import Lottie from 'lottie-web/build/player/lottie_light';
+//import spaceAnimation from '@/assets/Rocket_loader.json'; // lottie free animation
 
 const Header = () => {
-  const lottieRef = useRef<HTMLDivElement>(null);
+  // const lottieRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (!lottieRef.current) return;
+  // useEffect(() => {
+  //   if (!lottieRef.current) return;
 
-    const instance = Lottie.loadAnimation({
-      container: lottieRef.current, // 렌더링할 DOM 요소
-      renderer: 'svg', // 'svg' | 'canvas' | 'html'
-      loop: true,
-      autoplay: true,
-      animationData: spaceAnimation,
-    });
+  //   const instance = Lottie.loadAnimation({
+  //     container: lottieRef.current, // 렌더링할 DOM 요소
+  //     renderer: 'svg', // 'svg' | 'canvas' | 'html'
+  //     loop: true,
+  //     autoplay: true,
+  //     animationData: spaceAnimation,
+  //   });
 
-    return () => {
-      instance.destroy(); // 언마운트 시 메모리 해제
-    };
-  }, []);
+  //   return () => {
+  //     instance.destroy(); // 언마운트 시 메모리 해제
+  //   };
+  // }, []);
 
   return (
     <header className='layout_header'>
@@ -29,7 +29,7 @@ const Header = () => {
         <Link to='/' className=''>
           <span className=''>WORK</span>
           SPACE
-          <div className='lottie-space' ref={lottieRef}></div>
+          {/* <div className='lottie-space' ref={lottieRef}></div> */}
           <p>It’s a side project built with React, Vite, and Firebase.</p>
         </Link>
       </h1>
